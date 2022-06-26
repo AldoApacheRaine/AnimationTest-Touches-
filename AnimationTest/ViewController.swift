6093262db9b4deb4e643bbc2f849b8c1e2bb6796
Max Hmelev <mha93@yandex.ru>
2022-06-26 18:06:24 +0300

    created animation

--- AnimationTest/ViewController.swift
+++ AnimationTest/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    var dragging = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first?.location(in: self.view) {
+            print("touch \(touch)")
+                dragging = true
+        }
     }
 
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        dragging = false
+        if let touch = touches.first?.location(in: self.view) {
+            print("touchesEnded \(touch)")
+            if isTapedToImage(touch) {
+                UIView.animate(withDuration: 1) {
+                    self.imageView.frame.origin.x = touch.x - (self.imageView.frame.size.width/2)
+                    self.imageView.frame.origin.y = self.view.frame.maxY - self.imageView.frame.size.height
+                }
+            } else {
+                UIView.animate(withDuration: 1) {
+                    self.imageView.frame.origin.x = touch.x - (self.imageView.frame.size.width/2)
+                    self.imageView.frame.origin.y = touch.y - (self.imageView.frame.size.height/2)
+                }
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (dragging){
+            if let touch = touches.first?.location(in: self.view){
+                print("touchesMoved \(touch)")
+                imageView.frame.origin.x = touch.x - (imageView.frame.size.width/2)
+                imageView.frame.origin.y = touch.y - (imageView.frame.size.height/2)
+            }
+        }
+    }
 
+    private func isTapedToImage(_ touch: CGPoint) -> Bool {
+        imageView.frame.contains(touch) ? true : false
+    }
 }
 
