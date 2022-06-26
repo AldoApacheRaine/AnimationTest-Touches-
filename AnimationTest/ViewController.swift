a8b34c37a4299430fd11d445fa1ba0aa7c2eb02b
Max Hmelev <mha93@yandex.ru>
2022-06-26 18:12:10 +0300

    bugs fixed

--- AnimationTest/ViewController.swift
+++ AnimationTest/ViewController.swift
@@ -19,7 +19,9 @@ class ViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first?.location(in: self.view) {
             print("touch \(touch)")
+            if isTapedToImage(touch) {
                 dragging = true
+            }
         }
     }
 
